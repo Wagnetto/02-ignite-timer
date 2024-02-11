48dec5d4e5bbbe3de7e6a711d7f44981b3463954
Wagner Silveira Souto <wagner.souto@ifood.com.br>
2024-02-11 18:14:12 -0300

    Update inputs on Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,8 @@ import {
   FormContainer,
   Separator,
   StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
 } from './styles'
 
 export function Home() {
@@ -13,10 +15,28 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em: </label>
-          <input type="text" id="task" />
+          <TaskInput
+            type="text"
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para a tarefa"
+          />
+          <datalist id="task-suggestions">
+            <option value="pokédex"></option>
+            <option value="rocketseat"></option>
+            <option value="playground"></option>
+            <option value="banana"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante: </label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            step={5}
+            min={5}
+            max={60}
+            id="minutesAmount"
+            placeholder="00"
+          />
           <span>minutos.</span>
         </FormContainer>
         <CountDownContainer>
