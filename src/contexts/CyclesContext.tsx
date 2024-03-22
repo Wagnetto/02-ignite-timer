d5f9cf9d72c1fdc73dfaacc6bba27feac3b6a0a9
Wagner Silveira Souto <wagner.souto@ifood.com.br>
2024-03-22 09:43:38 -0300

    feature/Add mark as finished action type on reducer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -61,9 +61,19 @@ export function CyclesContextProvider({
           activeCycleId: null,
         }
       }
-      // if (action.type === 'MARK_CYCLE_AS_FINISHED') {
-      //   return [...state, action.payload.activeCycleId]
-      // }
+      if (action.type === 'MARK_CYCLE_AS_FINISHED') {
+        return {
+          ...state,
+          cycle: state.cycles.map((cycle) => {
+            if (cycle.id === state.activeCycleId) {
+              return { ...cycle, finishedDate: new Date() }
+            } else {
+              return cycle
+            }
+          }),
+          activeCycleId: null,
+        }
+      }
 
       return state
     },
